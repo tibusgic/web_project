05d24bef7cfc6b779ddfc305c97c47aa65417d46
Sard-Karma <ethan.jeanneau2@orange.fr>
2025-12-10 16:19:41 +0100

    v1.0.2 modif de star.js

--- star.js
+++ star.js
@@ -1,29 +1,13 @@
 import * as THREE from 'three';
 
-export const genratePlanet = (size, planetTexture, positionX, ring) => {
-  const planetGeometry = new THREE.SphereGeometry(size, 50, 50);
-  const planetMaterial = new THREE.MeshStandardMaterial({map: planetTexture});
+export const genrateStar = (size, starTexture, positionX) => {
+  const starGeometry = new THREE.SphereGeometry(size, 50, 50);
+  const starMaterial = new THREE.MeshStandardMaterial({map: starTexture});
 
-  const planet = new THREE.Mesh(planetGeometry, planetMaterial);
-  const planetObj = new THREE.Object3D();
-  planet.position.set(positionX, 0, 0);
+  const star = new THREE.Mesh(starGeometry, starMaterial);
+  const starObj = new THREE.Object3D();
+  star.position.set(positionX, 0, 0);
 
-  if (ring) {
-    const ringGeo = new THREE.RingGeometry(
-      ring.innerRadius,
-      ring.outerRadius,
-      32
-    );
-    const ringMat = new THREE.MeshBasicMaterial({
-      map: ring.ringmat,
-      side: THREE.DoubleSide,
-    });
-    const ringMesh = new THREE.Mesh(ringGeo, ringMat);
-    planetObj.add(ringMesh);
-    ringMesh.position.set(positionX, 0, 0);
-    ringMesh.rotation.positionX = -0.5 * Math.PI;
-  }
-
-  scene.add(planetObj);
-  planetObj.add(planet);
+  scene.add(starObj);
+  starObj.add(star);
 };
