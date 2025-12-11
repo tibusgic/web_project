574c1df8ff1705caa862a4dad679224a4e0ddab7
Sard-Karma <ethan.jeanneau2@orange.fr>
2025-12-11 12:08:00 +0100

    Ajout de l'utilisation de l'API

--- position.js
+++ position.js
@@ -1,1 +1,25 @@
-//Use kepler.js
+//Use api form : https://astrologyapi.docs.apiary.io/#reference
+
+var request = new XMLHttpRequest();
+
+request.open('POST', 'http://ephemeris.kibo.cz/api/v1/planets');
+
+request.setRequestHeader('Content-Type', 'application/json');
+request.setRequestHeader('Accept', 'application/json');
+
+request.onreadystatechange = function () {
+  if (this.readyState === 4) {
+    console.log('Status:', this.status);
+    console.log('Headers:', this.getAllResponseHeaders());
+    console.log('Body:', this.responseText);
+  }
+};
+
+var body = { 
+    'event': '20251210110000', 
+    'planets': ['Earth', 'Moon'],
+    'topo': [ longitude, latitude, geoalt],
+    'zodiac': 'sidereal mode name'
+};
+
+request.send(JSON.stringify(body));
