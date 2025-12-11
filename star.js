08944e1f870092f1dd636fd1164bcf0f216dacc7
Sard-Karma <ethan.jeanneau2@orange.fr>
2025-12-11 10:02:51 +0100

    v1.0.4

--- star.js
+++ star.js
@@ -1,13 +1,19 @@
 import * as THREE from 'three';
 
-export const genrateStar = (size, starTexture, positionX) => {
+export const genrateStar = (size, starTexture) => {
   const starGeometry = new THREE.SphereGeometry(size, 50, 50);
   const starMaterial = new THREE.MeshStandardMaterial({map: starTexture});
 
   const star = new THREE.Mesh(starGeometry, starMaterial);
   const starObj = new THREE.Object3D();
-  star.position.set(positionX, 0, 0);
+  star.position.set(0, 0, 0);
 
-  scene.add(starObj);
+  const sunLight = new THREE.PointLight(0xFFFFFF, 2, 3000); //Ajuster au besoin
+  sunLight.castShadow = true;
+
   starObj.add(star);
+  starObj.add(sunLight);
+  scene.add(starObj);
+
+  return starObj;
 };
